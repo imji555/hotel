f5f35f1d4f2c03012d5a984f76e0122bc6df9a84
imji555 <132755294+imji555@users.noreply.github.com>
2023-06-03 17:52:44 +0300

    change this file

--- models/account_move.py
+++ models/account_move.py
@@ -3,14 +3,4 @@
 from odoo import api, models
 
 
-class AccountMove(models.Model):
 
-    _inherit = "account.move"
-
-    @api.model
-    def create(self, vals):
-        res = super(AccountMove, self).create(vals)
-        if self._context.get("folio_id"):
-            folio = self.env["hotel.folio"].browse(self._context["folio_id"])
-            folio.write({"hotel_invoice_id": res.id, "invoice_status": "invoiced"})
-        return res
